ab8949b773ef91f2a91cc782767d726e5c154154
Alexander Lipatov <tellmeac@gmail.com>
2023-12-06 09:08:02 +0300

    Provide CORS support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/tellmeac/goalgo/internal/app"
 	"log"
@@ -15,5 +16,8 @@ func main() {
 	router.Path("/updates").Methods(http.MethodGet).HandlerFunc(delivery.GetUpdates)
 	router.Path("/chart").Methods(http.MethodGet).HandlerFunc(delivery.GetChart)
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	log.Fatal(http.ListenAndServe(":80", handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"}),
+	)(router)))
 }
--- go.mod
+++ go.mod
@@ -4,12 +4,16 @@ go 1.21.4
 
 require github.com/jmoiron/sqlx v1.3.5
 
-require github.com/mattn/go-sqlite3 v1.14.18
+require (
+	github.com/gorilla/handlers v1.5.2
+	github.com/gorilla/mux v1.8.1
+	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
