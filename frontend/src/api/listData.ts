71eb09984458ffed3a78163e9a6029c1df35e27f
Alexander Lipatov <tellmeac@gmail.com>
2023-12-10 10:44:12 +0300

    front fixes

--- frontend/src/api/listData.ts
+++
@@ -1,8 +0,0 @@
-import axios, { AxiosResponse } from 'axios';
-import { DataModel } from '../types/DataModel';
-
-export const listData = (): Promise<AxiosResponse<DataModel[]>> => {
-  // const startTime = {Date.now() - 2 * 24 * 60 * 60 * 1000} / 1000;
-  // return axios.get(`http://localhost:8080/api/updates?from=${startTime.toString()}`);
-  return axios.get('http://localhost:8080/api/updates?from=1701826800');
-}
